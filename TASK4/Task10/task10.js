0355584b5c113dfe57c4588fd4702b7f555638f5
ashihasv28 <ashihasv28@gmail.com>
2026-03-05 18:34:00 +0530

    Update task10.js

--- TASK4/Task10/task10.js
+++ TASK4/Task10/task10.js
@@ -1,10 +1,58 @@
-let textBox = document.querySelector('.textBox');
-let count = document.querySelector('.count');
+let inputs = [];
 
-textBox.oninput = function(){
+let container = document.querySelector('.container');
 
-  let length = textBox.value.length;
+let textarea = document.createElement('textarea');
 
-  count.innerText = 'Characters: ' + length;
+let submit = document.createElement('button');
+submit.innerText = 'Submit';
+
+let count = document.createElement('div');
+count.className = 'info';
+count.innerText = 'Characters: 0';
+
+let longest = document.createElement('div');
+longest.className = 'info';
+longest.innerText = 'Longest Input: ';
+
+container.appendChild(textarea);
+container.appendChild(submit);
+container.appendChild(count);
+container.appendChild(longest);
+
+textarea.oninput = function(){
+
+  let text = textarea.value;
+
+  count.innerText = 'Characters: ' + text.length;
+
+};
+
+submit.onclick = function(){
+
+  let text = textarea.value;
+
+  if(text !== ''){
+
+    inputs.push(text);
+
+    let long = '';
+
+    for(let i = 0; i < inputs.length; i++){
+
+      if(inputs[i].length > long.length){
+
+        long = inputs[i];
+
+      }
+
+    }
+
+    longest.innerText = 'Longest Input: ' + long;
+
+    textarea.value = '';
+    count.innerText = 'Characters: 0';
+
+  }
 
 };
