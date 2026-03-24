0a35cbaf3dce7397d601ec695f6fd2bad6a6ad90
ashihasv28 <ashihasv28@gmail.com>
2026-03-24 20:51:34 +0530

    Create numberLogger.js

---
+++ Node js/TASK1/Task3/numberLogger.js
@@ -0,0 +1,13 @@
+// numberLogger.js
+
+const fs = require('fs');
+
+function logNumber(number) {
+  fs.appendFile('numbers.txt', number + '\n', (err) => {
+    if (err) {
+      console.error("Error writing to file:", err);
+    }
+  });
+}
+
+module.exports = { logNumber };
