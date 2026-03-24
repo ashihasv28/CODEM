e17b9e69b3e1516ce20c614a0a2130a142e4745e
ashihasv28 <ashihasv28@gmail.com>
2026-03-24 20:38:06 +0530

    Create app.js

---
+++ Node js/TASK1/Task2/app.js
@@ -0,0 +1,17 @@
+// app.js
+
+const fs = require('fs');
+
+fs.readFile('users.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error("Error reading file:", err);
+    return;
+  }
+
+  const users = data.split('\n');
+
+  console.log("User List:");
+  users.forEach((user, index) => {
+    console.log(`${index + 1}. ${user}`);
+  });
+});
