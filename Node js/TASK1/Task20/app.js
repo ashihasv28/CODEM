37f667f5e80bbdc78a6e320a34527d6f11d063d3
ashihasv28 <ashihasv28@gmail.com>
2026-04-06 22:03:17 +0530

    Update app.js

--- Node js/TASK1/Task20/app.js
+++ Node js/TASK1/Task20/app.js
@@ -1,6 +1,4 @@
 const fs = require('fs');
-
-// Read JSON file
 fs.readFile('products.json', 'utf8', (err, data) => {
   if (err) {
     console.error("Error reading file:", err);
@@ -9,7 +7,6 @@ fs.readFile('products.json', 'utf8', (err, data) => {
 
   const products = JSON.parse(data);
 
-  // Print output
   console.log("Product List");
   products.forEach(product => {
     console.log(`${product.name} - $${product.price}`);
