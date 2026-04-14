75b40d5c763e9c88ba3d55408c464b6ff5d28852
ashihasv28 <ashihasv28@gmail.com>
2026-04-14 14:52:29 +0530

    Create productRoutes.js

---
+++ Node js/TASK4/Task24/routes/productRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const logger = require('../middleware/logger');
+const productController = require('../controllers/productController');
+
+router.use(logger);
+
+router.get('/', productController.getProducts);
+router.get('/:id', productController.getProductById);
+router.post('/', productController.createProduct);
+
+module.exports = router;
